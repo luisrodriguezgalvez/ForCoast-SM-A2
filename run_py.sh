924d22e1b06f3d6d821c11523e2316662644b4f0
Arthur <arthurcapet@gmail.com>
2021-10-29 19:30:20 +0200

    added run_py.sh for tests outside of docker

---
+++ run_py.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Example: run.sh eforie 2021-09-20 10 [28.6803,44.2201] [28.6611,44.0535,28.6617,44.0553] 
+# Example: run.sh eforie 2021-09-20 10 [28.7303,44.2201] [28.6611,44.0535,28.6617,44.0553]
+
+#source activate env
+
+INITIAL_DIR="$(pwd)"
+
+# Clean data folder
+#rm /usr/src/app/data/*.*
+
+# Download data (using data dir from yml)
+cd ./PreProcessing
+echo "python forcoast_download_yml.py -a $1 -T $2 -p $3"
+python forcoast_download_yml.py -a $1 -T $2 -p $3 
+# Run model
+
+cd ../Processing
+echo "python forcoast.py -y $1 -T $2 -p $3 -s $4 -t $5"
+python forcoast.py -y $1 -T $2 -p $3 -s $4 -t $5
+# Run postprocessing (uses data directory from yaml file)
+cd ../Postprocessing
+echo "python SM-A2-Postprocess.py -y $1.yaml -s $4 -t $5"
+python SM-A2-Postprocess.py -y $1.yaml -s $4 -t $5
+
+# Run bulletin spcript (uses data directory from yaml file)
+cd ../Bulletinscript
+echo "bulletin_script.py -y $1.yaml -s $4 -t $5"
+python bulletin_script.py -y $1.yaml -s $4 -t $5
+
+echo $INITIAL_DIR
+cd ..
+ls
+
+#cp /usr/src/app/data/*.png $INITIAL_DIR
