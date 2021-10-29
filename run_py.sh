b2139866bec3c65d7938270fb6efcdb2fd761e27
Arthur <arthurcapet@gmail.com>
2021-10-29 19:46:51 +0200

    run_py.sh, for local tests

--- run_py.sh
+++ run_py.sh
@@ -9,24 +9,30 @@ INITIAL_DIR="$(pwd)"
 # Clean data folder
 #rm /usr/src/app/data/*.*
 
+DATA_DIR=/home/arthur/Desktop/DOCS/PROJECTS/FORECOAST/Pilot/SM/ForCoast-SM-A3/testdata/
+
+mkdir ${DATA_DIR}
+
 # Download data (using data dir from yml)
 cd ./PreProcessing
 echo "python forcoast_download_yml.py -a $1 -T $2 -p $3"
-python forcoast_download_yml.py -a $1 -T $2 -p $3 
-# Run model
+python forcoast_download_yml.py -a $1 -T $2 -p $3 -d ${DATA_DIR}
 
+# Run model
 cd ../Processing
 echo "python forcoast.py -y $1 -T $2 -p $3 -s $4 -t $5"
-python forcoast.py -y $1 -T $2 -p $3 -s $4 -t $5
+python forcoast.py -y $1 -T $2 -p $3 -s $4 -t $5 -d ${DATA_DIR}
+
 # Run postprocessing (uses data directory from yaml file)
-cd ../Postprocessing
+
+cd ../PostProcessing
 echo "python SM-A2-Postprocess.py -y $1.yaml -s $4 -t $5"
-python SM-A2-Postprocess.py -y $1.yaml -s $4 -t $5
+python SM-A2-Postprocess.py -y $1.yaml -s $4 -t $5 -d ${DATA_DIR}
 
 # Run bulletin spcript (uses data directory from yaml file)
 cd ../Bulletinscript
 echo "bulletin_script.py -y $1.yaml -s $4 -t $5"
-python bulletin_script.py -y $1.yaml -s $4 -t $5
+python bulletin_script.py -y $1.yaml -s $4 -t $5 -d ${DATA_DIR}
 
 echo $INITIAL_DIR
 cd ..
