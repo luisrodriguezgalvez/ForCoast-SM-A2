c18d7b758975c850cb40e5729251c76522854a28
danieltwigt <50665134+danieltwigt@users.noreply.github.com>
2021-11-04 21:20:06 +0100

    Fixed command line input and some minor changes
    Fixed command line input and some minor changes

--- test.bat
+++ test.bat
@@ -4,7 +4,7 @@ python forcoast_download_yml.py -a eforie -T 2021-09-10 -p 20 -d c:\data
 cd ..
 # Run model (uses data directory from command line, such that we don't need to change the yaml file for testing)
 cd  Processing
-python forcoast.py -y eforie -T 2021-09-20 -p 10 -s [28.6803,44.2201] -t [28.6611,44.0535,28.6617,44.0553] -d c:\data 
+python forcoast.py -y pilot7 -T 2021-09-20 -p 10 -s [28.6803,44.2201] -t [28.6611,44.0535,28.6617,44.0553] -d c:\data\ 
 cd ..
 # Run postprocessing (uses data directory from command line, such that we don't need to change the yaml file for testing)
 cd PostProcessing
