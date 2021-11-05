2445043db2fcd375732ad53fcbe4136d1752801c
Arthur <arthurcapet@gmail.com>
2021-11-05 13:12:51 +0100

    updated download.yaml for the third mesh_mask and BS files, harmonized eforie.yaml in that regard

--- run_py.sh
+++ run_py.sh
@@ -15,23 +15,23 @@ mkdir ${DATA_DIR}
 
 # Download data (using data dir from yml)
 cd ./PreProcessing
-echo "python forcoast_download_yml.py -a $1 -T $2 -p $3"
-python forcoast_download_yml.py -a $1 -T $2 -p $3 -d ${DATA_DIR}
+#echo "python forcoast_download_yml.py -a $1 -T $2 -p $3 -d ${DATA_DIR}"
+#python forcoast_download_yml.py -a $1 -T $2 -p $3 -d ${DATA_DIR}
 
 # Run model
 cd ../Processing
-echo "python forcoast.py -y $1 -T $2 -p $3 -s $4 -t $5"
+echo "python forcoast.py -y $1 -T $2 -p $3 -s $4 -t $5 -d ${DATA_DIR} "
 python forcoast.py -y $1 -T $2 -p $3 -s $4 -t $5 -d ${DATA_DIR}
 
 # Run postprocessing (uses data directory from yaml file)
 
 cd ../PostProcessing
-echo "python SM-A2-Postprocess.py -y $1.yaml -s $4 -t $5"
+echo "python SM-A2-Postprocess.py -y $1.yaml -s $4 -t $5 -d ${DATA_DIR}"
 python SM-A2-Postprocess.py -y $1.yaml -s $4 -t $5 -d ${DATA_DIR}
 
 # Run bulletin spcript (uses data directory from yaml file)
-cd ../Bulletinscript
-echo "bulletin_script.py -y $1.yaml -s $4 -t $5"
+cd ../BulletinScript
+echo "bulletin_script.py -y $1.yaml -s $4 -t $5 -d ${DATA_DIR}"
 python bulletin_script.py -y $1.yaml -s $4 -t $5 -d ${DATA_DIR}
 
 echo $INITIAL_DIR
