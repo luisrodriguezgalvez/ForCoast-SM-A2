bf182e90e7d7bb1f5296006c81ee77c8e6e7bddd
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2021-11-22 11:27:02 +0100

    both bulletin scripts

--- BulletinScript/bulletin_script.py
+++ BulletinScript/bulletin_script.py
@@ -1,40 +1,37 @@
-#import PIL
-from PIL import Image, ImageDraw,ImageFont 
+import PIL
+from PIL import Image, ImageDraw,ImageFont
+import os
 import os.path, time
 import sys, getopt
 import yaml
-import csv
+import glob
 
-argv = sys.argv[1:]
-try:
-    opts, args = getopt.getopt(argv,"hy:s:c:t:k:d:",["yamlfile="])
-except getopt.GetoptError:
-    print(opts)
-    print ('USAGE : SM-A2-Postprocess.py -y <username>')
-    sys.exit(2)
-for opt, arg in opts:
-    if opt == '-h':
-        print ('USAGE : SM-A2-Postprocess.py -y <username>')
-        sys.exit()
-    elif opt in ("-y", "--yamlfile"):
-        USER_YAML_FILE = arg
-    elif opt in ("-s"):
-        # AC 10112021 - Not sure this is a safe parsing method .. Same below.
-        exec('source = '+arg)
-    elif opt in ("-c"):
-        sourcecount = int(arg)
-    elif opt in ("-t"):
-        exec('target = '+arg)
-    elif opt in ("-k"):
-        targetcount = int(arg)
-    elif opt in ("-d"): # AC 10112021 shouldn't be needed ? 
-        datadir = arg
-USER_YAML_FILE = '../usr/' + USER_YAML_FILE + '/config/config.yaml'
-print ('yaml file is', USER_YAML_FILE)
+# argv = sys.argv[1:]
 
-with open(USER_YAML_FILE) as f:
+# try:
+#     opts, args = getopt.getopt(argv,"hy:d:s:t:",["yamlfile="])
+# except getopt.GetoptError:
+#     print ('bulletin_script.py -y <yamlfile>')
+#     sys.exit(2)
+# for opt, arg in opts:
+#     if opt == '-h':
+#         print ('bulletin_script.py -y <yamlfile>')
+#         sys.exit()
+#     elif opt in ("-y", "--yamlfile"):
+#         USER_YAML_FILE = arg
+#     elif opt in ("-d"):
+#         datadir = arg
+#     elif opt in ("-s"):
+#         source = arg
+#     elif opt in ("-t"):
+#         target = arg
+
+with open("../Processing/pilot7.yaml") as f:
     data = yaml.load(f, Loader=yaml.FullLoader)
-    figdir        = '../usr/'+data['username'] +'/output/target_' + str(targetcount)+'_source_' + str(sourcecount)+'/'
+    fdate=data['sdate']
+    fx0=data['x0']
+    fy0=data['y0']
+    farea=data['experiment']
 
     #fdate=data['sdate']
     #fx0=data['x0']
@@ -48,18 +45,18 @@ img_violin    = Image.open(figdir+'TS_violin.png')
 img_risk      = Image.open(figdir+'TS_Risk.png')
 img_riskchart = Image.open(figdir+'TS_Risk_chart.png')
 
-risktab=[]
-# Here we need to select the maps for the worst case
-with open(figdir+'Risk.csv', 'r') as f:
-    reader = csv.reader(f, delimiter='\t') # LR 1. delimiter='\t' does not find tabs in Risk.csv and 2. the blank in-between rows are deleted (manually) from Risk.csv
-    for row in reader: # each row is a list
-        risktab.append(row)
+img1 = PIL.Image.open('TS_Alarm.png')
+img2 = PIL.Image.open('FORCOAST_Logo_WhiteBack.png')
+img3 = PIL.Image.open('map_particles.png')
+img4 = PIL.Image.open('FORCOAST_Footer_Blue.png')
 
 riskval  = [ float(v[1]) for v in risktab]
 imaxrisk = riskval.index(max(riskval))
 
-img_map = Image.open(figdir+'AllTracks_Alarm'+str(imaxrisk)+'.png')
-img_logo = Image.open('./FORCOAST_Logo_WhiteBack.png')
+img1Width, img1Height = img1.size
+img2Width, img2Height = img2.size
+img3Width, img3Height = img3.size
+img4Width, img4Height = img4.size
 
 img_violin_Width   , img_violin_Height    = img_violin.size
 img_risk_Width     , img_risk_Height      = img_risk.size
@@ -67,18 +64,57 @@ img_riskchart_Width, img_riskchart_Height = img_riskchart.size
 img_map_Width      , img_map_Height       = img_map.size
 img_logo_Width     , img_logo_Height      = img_logo.size
 
-newImg = Image.new('RGBA', (img_violin_Width + img_map_Width, img_violin_Height + img_risk_Height), (255, 255, 255))
+img1_fixed_height = int((img1Height + img3Height) / 2)
 
-newImg.paste(img_violin   , (0                , 0))
-newImg.paste(img_map      , (img_violin_Width , 0))
-newImg.paste(img_risk     , (0                , img_violin_Height))
-newImg.paste(img_riskchart, (img_violin_Width , img_violin_Height))
+img1_height_percent = (img1_fixed_height / float(img1.size[1]))
+img1_width_size = int((float(img1.size[0]) * float(img1_height_percent)))
+img1_r = img1.resize((img1_width_size, img1_fixed_height), PIL.Image.NEAREST)
+# img1_r.save('img1_resized.png')
 
-font_path = "ariali.ttf"
-font = ImageFont.truetype(font_path, 20)
+
+img3_fixed_height = int((img1Height + img3Height) / 2.2)
+
+img3_height_percent = (img3_fixed_height / float(img3.size[1]))
+img3_width_size = int((float(img3.size[0]) * float(img3_height_percent)))
+img3_r = img3.resize((img3_width_size, img3_fixed_height), PIL.Image.NEAREST)
+# img3_r.save('img3_resized.png')
+
+
+img4_fixed_width = int(img1_width_size + img3_width_size)
+
+img4_width_percent = (img4_fixed_width / float(img4.size[0]))
+img4_height_size = int((float(img4.size[1]) * float(img4_width_percent)))
+img4_r = img4.resize((img4_fixed_width, img4_height_size), PIL.Image.NEAREST)
+# img4_r.save('footer_resized.png')
+
+
+newImg = Image.new('RGBA', (img1_width_size + img3_width_size, img1_fixed_height + img2Height + img4_height_size), (255, 255, 255))
+
+newImg.paste(img1_r, (0, img2Height))
+newImg.paste(img2, (20, 20))
+newImg.paste(img3_r, (img1_width_size, img2Height + int((img1_fixed_height - img3_fixed_height) / 2)))
+newImg.paste(img4_r, (0, (img2Height + img1_fixed_height)))
+
+font_path_1 = "ariali.ttf"
+font_1 = ImageFont.truetype(font_path_1, 26)
+
+font_path_2 = "arialbd.ttf"
+font_2 = ImageFont.truetype(font_path_2, 40)
 
 # print("last modified: %s" % time.ctime(os.path.getmtime(file)))
-filecreated = time.ctime(os.path.getctime(figdir+'TS_violin.png'))
+filecreated = time.ctime(os.path.getctime('TS_Alarm.png'))
+
+draw = PIL.ImageDraw.Draw(newImg)
+draw.text(((img2Height + 410, img2Height/2.6)), ('LAND POLLUTION SERVICE'), font=font_2,fill=(23,111,176,255))
+draw.text((img1_width_size + img3_width_size - 600, img2Height/1), ('Bulletin generated on: ' + filecreated), font=font_1,fill=(0,0,0,255))
+draw.text((img1_width_size + img3_width_size - 600, img2Height/1.4), ('Release date: ' + fdate), font=font_1,fill=(0,0,0,255))
+draw.text((img1_width_size + img3_width_size - 600, img2Height/2.1), ('Discharge point: x = ' + str(fx0[0]) + ' ; ' + 'y = ' + str(fy0[0])), font=font_1,fill=(0,0,0,255))
+draw.text((img1_width_size + img3_width_size - 600, img2Height/4), ('Area: ' + farea), font=font_1,fill=(0,0,0,255))
+
+newImg.save("bulletin_with_header3.png", quality=95)
+
+
+
 
 #draw = ImageDraw.Draw(newImg)
 #draw.text((img1Width-400, img2Height/1)  , filecreated, font=font,fill=(0,0,0,255))
@@ -86,7 +122,7 @@ filecreated = time.ctime(os.path.getctime(figdir+'TS_violin.png'))
 #draw.text((img1Width-400, img2Height/1.9), ('x = ' + str(fx0[0]) + ' ' + 'y = ' + str(fy0[0])), font=font,fill=(0,0,0,255))
 #draw.text((img1Width-400, img2Height/2.5), farea, font=font,fill=(0,0,0,255))
 
-newImg.save(figdir+"bulletin.png", quality=95)
+# newImg.save(figdir+"bulletin.png", quality=95)
 '''
 
 # draw = PIL.ImageDraw.Draw(img)
---
+++ BulletinScript/bulletin_script_Arthur.py
@@ -0,0 +1,95 @@
+#import PIL
+from PIL import Image, ImageDraw,ImageFont 
+import os.path, time
+import sys, getopt
+import yaml
+import csv
+
+argv = sys.argv[1:]
+try:
+    opts, args = getopt.getopt(argv,"hy:s:c:t:k:d:",["yamlfile="])
+except getopt.GetoptError:
+    print(opts)
+    print ('USAGE : SM-A2-Postprocess.py -y <username>')
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == '-h':
+        print ('USAGE : SM-A2-Postprocess.py -y <username>')
+        sys.exit()
+    elif opt in ("-y", "--yamlfile"):
+        USER_YAML_FILE = arg
+    elif opt in ("-s"):
+        # AC 10112021 - Not sure this is a safe parsing method .. Same below.
+        exec('source = '+arg)
+    elif opt in ("-c"):
+        sourcecount = int(arg)
+    elif opt in ("-t"):
+        exec('target = '+arg)
+    elif opt in ("-k"):
+        targetcount = int(arg)
+    elif opt in ("-d"): # AC 10112021 shouldn't be needed ? 
+        datadir = arg
+USER_YAML_FILE = '../usr/' + USER_YAML_FILE + '/config/config.yaml'
+print ('yaml file is', USER_YAML_FILE)
+
+with open(USER_YAML_FILE) as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
+    figdir        = '../usr/'+data['username'] +'/output/target_' + str(targetcount)+'_source_' + str(sourcecount)+'/'
+
+    #fdate=data['sdate']
+    #fx0=data['x0']
+    #fy0=data['y0']
+    #farea=data['experience']
+
+#img1 = PIL.Image.open('TS_Alarm.png')
+#img2 = PIL.Image.open('FORCOAST_Logo_WhiteBack.png')
+
+img_violin    = Image.open(figdir+'TS_violin.png')
+img_risk      = Image.open(figdir+'TS_Risk.png')
+img_riskchart = Image.open(figdir+'TS_Risk_chart.png')
+
+risktab=[]
+# Here we need to select the maps for the worst case
+with open(figdir+'Risk.csv', 'r') as f:
+    reader = csv.reader(f, delimiter='\t') # LR 1. delimiter='\t' does not find tabs in Risk.csv and 2. the blank in-between rows are deleted (manually) from Risk.csv
+    for row in reader: # each row is a list
+        risktab.append(row)
+
+riskval  = [ float(v[1]) for v in risktab]
+imaxrisk = riskval.index(max(riskval))
+
+img_map = Image.open(figdir+'AllTracks_Alarm'+str(imaxrisk)+'.png')
+img_logo = Image.open('./FORCOAST_Logo_WhiteBack.png')
+
+img_violin_Width   , img_violin_Height    = img_violin.size
+img_risk_Width     , img_risk_Height      = img_risk.size
+img_riskchart_Width, img_riskchart_Height = img_riskchart.size
+img_map_Width      , img_map_Height       = img_map.size
+img_logo_Width     , img_logo_Height      = img_logo.size
+
+newImg = Image.new('RGBA', (img_violin_Width + img_map_Width, img_violin_Height + img_risk_Height), (255, 255, 255))
+
+newImg.paste(img_violin   , (0                , 0))
+newImg.paste(img_map      , (img_violin_Width , 0))
+newImg.paste(img_risk     , (0                , img_violin_Height))
+newImg.paste(img_riskchart, (img_violin_Width , img_violin_Height))
+
+font_path = "ariali.ttf"
+font = ImageFont.truetype(font_path, 20)
+
+# print("last modified: %s" % time.ctime(os.path.getmtime(file)))
+filecreated = time.ctime(os.path.getctime(figdir+'TS_violin.png'))
+
+#draw = ImageDraw.Draw(newImg)
+#draw.text((img1Width-400, img2Height/1)  , filecreated, font=font,fill=(0,0,0,255))
+#draw.text((img1Width-400, img2Height/1.4), fdate, font=font,fill=(0,0,0,255))
+#draw.text((img1Width-400, img2Height/1.9), ('x = ' + str(fx0[0]) + ' ' + 'y = ' + str(fy0[0])), font=font,fill=(0,0,0,255))
+#draw.text((img1Width-400, img2Height/2.5), farea, font=font,fill=(0,0,0,255))
+
+newImg.save(figdir+"bulletin.png", quality=95)
+'''
+# draw = PIL.ImageDraw.Draw(img)
+# draw.text((100, 100),"Sample Text")
+# img1.paste(img2)
+# img1.save("combined.png", quality=45)
+'''
